1942650777a1bf62fc9cede1620384e7e0df78ee
Sahil Johari <darewithsj@gmail.com>
2019-10-20 16:35:13 -0400

    Unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('Initialization', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
 
-it('contains all the child components', () => {
-  const wrapper = mount(<App />);
-  expect(wrapper.find('HeaderNav')).toBeTruthy();
-})
+describe('Component structure', () => {
+  it('contains all the child components', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('HeaderNav')).toBeTruthy();
+    expect(wrapper.find('IntroSection')).toBeTruthy();
+  })
+});
