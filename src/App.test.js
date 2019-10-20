9a208ebaa3d1a317bacbe6c364b9447c7a0e8e27
Sahil Johari <darewithsj@gmail.com>
2019-10-20 16:19:33 -0400

    Unit test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('contains all the child components', () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.find('HeaderNav')).toBeTruthy();
+})
