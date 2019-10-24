600bd72157dee898ab6c7b5c33ecca942cdb3b79
Sahil Johari <darewithsj@gmail.com>
2019-10-24 19:12:22 -0400

    Added breadcrumb for responsive nav

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <IntroSection />
       <HeaderNav />
+      <main style={{marginTop: "3rem"}}>
+        <IntroSection />
+      </main>
     </div>
   );
 }
--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { content } from './content';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './style.css';
 
-const HeaderNav = () => {
+const HeaderNav = props => {
     return (
-        <div className="navbar">
-            <div className="navItems">
-                {
-                    content.navItems.map((nav, i) => (
-                        <div key={i}><a href="/">{nav}</a></div>
-                    ))
-                }
-            </div>
-            <div className="links">
-                [placeholder for links]
-            </div>
-        </div>
+        <header className="navbar">
+            <nav className="navbar__navigation">
+                <DrawerToggleButton />
+                <div className="navbar__items">
+                    {
+                        content.navItems.map((nav, i) => (
+                            <div key={i}><a href="/">{nav}</a></div>
+                        ))
+                    }
+                </div>
+                <div className="spacer" />
+                <div className="links">
+                    [placeholder for links]
+                </div>
+            </nav>
+        </header>
     );
 };
 
