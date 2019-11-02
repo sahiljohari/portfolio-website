f1658f028ec6259765e6666a60a12535e87dc263
Sahil Johari <darewithsj@gmail.com>
2019-11-02 15:28:40 -0400

    Content updated

--- src/components/IntroSection/content.js
+++ src/components/IntroSection/content.js
@@ -2,5 +2,6 @@ export const content = {
     greeting: "Hi, my name is",
     introText: "",
     name: "Sahil Johari",
-    description: "I'm a software engineer based in Pittsburgh, PA specializing in building (and occasionally designing) exceptional, high-quality websites and applications."
+    description: "I'm a software engineer based in Pittsburgh, PA specializing in building (and occasionally designing) exceptional, high-quality websites and applications.",
+    buttonTitle: "Download Resume"
 };
