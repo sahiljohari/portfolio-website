0d7b16eecc84800fa4cdd6809eb5a197356bd5e0
Sahil Johari <darewithsj@gmail.com>
2019-11-03 03:30:08 -0500

    Image added

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -4,18 +4,24 @@ import './style.css';
 
 const AboutSection = () => {
     return (
-        <>
+        <div className='root'>
             <div className='sectionTitle'>{content.sectionTitle}</div>
-            <div className='about'>
-                <div className='content'>
+            <div className='content'>
+                <div className='display-img'>
+                    <img src="assets/Me.JPG" alt="profile" />
+                </div>
+                <div className='content-text'>
                     <p>{content.introParagraph}</p>
                     <p>{content.backgroundParagraph}</p>
-                </div>
-                <div className='display-img'>
-                    <p>[placeholder for image]</p>
+                    <p>{content.technologies.titleText}</p>
+                    <div className='tech-list'>
+                        <ul>
+                            {content.technologies.techStack.map((t, i) => (<li key={i}>{t}</li>))}
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
