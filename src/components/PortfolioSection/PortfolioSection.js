e716a9a1d3ff56b9227b1ce5215a0eb10817965a
Sahil Johari <darewithsj@gmail.com>
2020-06-27 22:57:42 -0400

    Work experience section revamped

--- src/components/PortfolioSection/PortfolioSection.js
+++ src/components/PortfolioSection/PortfolioSection.js
@@ -5,23 +5,33 @@ import "./style.css";
 
 const PortfolioSection = () => {
   const { sectionTitle, workExperience } = content;
+  const showSvg = false;
   return (
     <div className="portfolio-container">
       <h2 className="sectionTitle">{sectionTitle}</h2>
       <div className="work-experience-container">
-        <SVG
-          className={"portfolio-svg"}
-          src={"assets/svgs/undraw_profile_6l1l.svg"}
-        />
+        {showSvg && (
+          <SVG
+            className={"portfolio-svg"}
+            src={"assets/svgs/undraw_profile_6l1l.svg"}
+          />
+        )}
         <div className="work-experience-section">
           {workExperience.map((exp, i) => (
             <div className="work-exp-card" key={i}>
-              <div>
+              <div className="work-exp-card-header">
                 <h3 className="work-exp-company-name">{exp.company}</h3>
-                <h4 className="work-exp-role-name">{exp.role}</h4>
-                <label>{exp.period}</label>
+                <div className="work-exp-card-subheader">
+                  <h4 className="work-exp-role-name">{exp.role}</h4>
+                  <label>{exp.period}</label>
+                </div>
               </div>
               <p>{exp.responsibilities}</p>
+              <div className="tags">
+                {exp.tags.map((tag, idx) => (
+                  <div className="tag">{tag}</div>
+                ))}
+              </div>
             </div>
           ))}
         </div>
--- src/components/PortfolioSection/content.js
+++ src/components/PortfolioSection/content.js
@@ -1,15 +1,27 @@
 export const content = {
-  sectionTitle: "My Portfolio",
+  sectionTitle: "Work Experience",
   workExperience: [
     {
       company: "Rivers Agile",
       role: "Software Engineer",
       period: "August 2019 - Present",
       responsibilities:
-        "Working in an agile environment with over 35 successfully completed user stories so far, " +
+        "Working in an agile environment with over 48 successfully completed user stories so far, " +
         "my role is to develop front end modules on React for smooth and aesthetically pleasing user experience " +
         "including unit tests for UI components using Jest and Enzyme. " +
-        "I also collaborate with other developers from front-end, back-end, and UI/UX designers to create exciting and rewarding product features."
+        "I also collaborate with other developers from front-end, back-end, and UI/UX designers to create exciting and rewarding product features.",
+      tags: [
+        "React",
+        "Redux",
+        "SASS",
+        "Typescript",
+        "Node",
+        "Jest",
+        "Enzyme",
+        "Cypress",
+        "Ruby on rails",
+        "Git",
+      ],
     },
     {
       company: "Capgemini Technology Services India Ltd.",
@@ -18,8 +30,18 @@ export const content = {
       responsibilities:
         "Reduced the lead time by 80% for the customer via generating and distributing over 125 reports " +
         "by developing an automation web application using .NET framework. " +
-        "Enhanced the user experience by migrating legacy front-end code to newer technologies such as jQuery and Bootstrap."
-    }
+        "Enhanced the user experience by migrating legacy front-end code to newer technologies such as jQuery and Bootstrap.",
+      tags: [
+        ".NET",
+        "C#",
+        "ASP.NET",
+        "HTML5/CSS3",
+        "JavaScript",
+        "jQuery",
+        "Bootstrap",
+        "Oracle PL-SQL",
+      ],
+    },
   ],
   projects: [
     {
@@ -27,21 +49,21 @@ export const content = {
       projectName: "Document Organizer",
       projectSummary:
         "A simple dashboard-like web application that assists the user in maintaining valid status of their important documents, such as Passport, Visa, Drivers License, and many more, and notify them for timely renewals.",
-      externalLink: "https://github.com/sahiljohari/doc-tracker"
+      externalLink: "https://github.com/sahiljohari/doc-tracker",
     },
     {
       projectId: 2,
       projectName: "Game for Intelligent Simulated Military Opponents",
       projectSummary:
         "GISMO simulates a battlefield in which two rival forces compete to win. Using neuro-evolution, the tanks in the game determine their movement and direction of firing, evolving gradually to perform their tasks.",
-      externalLink: "https://github.com/sahiljohari/gismo"
+      externalLink: "https://github.com/sahiljohari/gismo",
     },
     {
       projectId: 3,
       projectName: "Playing Snake using Deep Reinforcement Learning",
       projectSummary:
         "Implemented a type of reinforcement learning algorithm called Q-learning to model an agent which plays the game of Snake.",
-      externalLink: "https://github.com/sahiljohari/thisthinglearns"
+      externalLink: "https://github.com/sahiljohari/thisthinglearns",
     },
     {
       projectId: 4,
@@ -49,7 +71,7 @@ export const content = {
         "Partitioning Random Geometric Graphs into Bipartite Subgraphs",
       projectSummary:
         "Implemented an algorithm that models ad-hoc networks of wireless sensors of varying scale across a variety of geographic areas.",
-      externalLink: "https://github.com/sahiljohari/wsn"
-    }
-  ]
+      externalLink: "https://github.com/sahiljohari/wsn",
+    },
+  ],
 };
