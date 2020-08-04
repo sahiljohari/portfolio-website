21f20f15266452a5e7b331db5daeb5756b5babe3
Sahil Johari <sjohari@riversagile.com>
2020-08-04 17:07:04 -0400

    Initial redesign for Work section

--- src/components/WorkSection/WorkSection.js
+++ src/components/WorkSection/WorkSection.js
@@ -1,45 +1,28 @@
-import React from "react";
-import { ReactSVG as SVG } from "react-svg";
-import { content } from "./content";
-import "./style.css";
+import React, { Fragment } from 'react';
+import { ReactSVG as SVG } from 'react-svg';
+import WorkCard from './WorkCard';
+import { content } from './content';
+import './style.css';
 
 const WorkSection = () => {
-  const { sectionTitle, workExperience } = content;
-  const showSvg = false;
-  return (
-    <div className="work-container">
-      <h2 className="workSectionTitle">{sectionTitle}</h2>
-      <div className="work-experience-container">
-        {showSvg && (
-          <SVG
-            className={"work-svg"}
-            src={"assets/svgs/undraw_profile_6l1l.svg"}
-          />
-        )}
-        <div className="work-experience-section">
-          {workExperience.map((exp, i) => (
-            <div className="work-exp-card" key={i}>
-              <div className="work-exp-card-header">
-                <h3 className="work-exp-company-name">{exp.company}</h3>
-                <div className="work-exp-card-subheader">
-                  <h4 className="work-exp-role-name">{exp.role}</h4>
-                  <label>{exp.period}</label>
-                </div>
-              </div>
-              <p>{exp.responsibilities}</p>
-              <div className="tags">
-                {exp.tags.map((tag, idx) => (
-                  <div key={idx} className="tag">
-                    {tag}
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	const { bannerText, sectionTitle, workExperience } = content;
+	return (
+		<div className="work">
+			<div className="work-banner">
+				<SVG className="work-svg" src={'assets/svgs/work-page-svg.svg'} />
+				<div className="left-side" dangerouslySetInnerHTML={{ __html: bannerText }}></div>
+			</div>
+			<div className="work-content">
+				<h2 className="work-section-title">{sectionTitle}</h2>
+				{workExperience.map((exp, i) => (
+					<Fragment key={i}>
+						<WorkCard workData={exp} />
+						{i !== workExperience.length - 1 && <hr />}
+					</Fragment>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default WorkSection;
--- src/components/WorkSection/content.js
+++ src/components/WorkSection/content.js
@@ -1,46 +1,30 @@
 export const content = {
-  sectionTitle: "Work Experience",
-  workExperience: [
-    {
-      company: "Rivers Agile",
-      role: "Software Engineer",
-      period: "August 2019 - Present",
-      responsibilities:
-        "Working in an agile environment with over 48 successfully completed user stories so far, " +
-        "my role is to develop front end modules on React for smooth and aesthetically pleasing user experience " +
-        "including unit tests for UI components using Jest and Enzyme. " +
-        "I also collaborate with other developers from front-end, back-end, and UI/UX designers to create exciting and rewarding product features.",
-      tags: [
-        "React",
-        "Redux",
-        "SASS",
-        "Typescript",
-        "Node",
-        "Jest",
-        "Enzyme",
-        "Cypress",
-        "Ruby on rails",
-        "Git",
-      ],
-    },
-    {
-      company: "Capgemini Technology Services India Ltd.",
-      role: "Senior Analyst",
-      period: "June 2015 - June 2017",
-      responsibilities:
-        "Reduced the lead time by 80% for the customer via generating and distributing over 125 reports " +
-        "by developing an automation web application using .NET framework. " +
-        "Enhanced the user experience by migrating legacy front-end code to newer technologies such as jQuery and Bootstrap.",
-      tags: [
-        ".NET",
-        "C#",
-        "ASP.NET",
-        "HTML5/CSS3",
-        "JavaScript",
-        "jQuery",
-        "Bootstrap",
-        "Oracle PL-SQL",
-      ],
-    },
-  ]
+	bannerText: '<div>simple.</div><div>elegant.</div><div>effective.</div><div>rewarding.</div>',
+	sectionTitle: "Here's what I have done so far",
+	workExperience: [
+		{
+			company: 'Rivers Agile',
+			logo: 'assets/images/ra-logo.png',
+			role: 'Software Engineer',
+			period: ' August 2019 - Present',
+			location: 'Pittsburgh, PA',
+			responsibilities:
+				'<p>Right on Day One at Rivers Agile, I joined a diverse team of talented and experienced developers on a large scale project with IBM Pittsburgh Lab. Our goal was to develop and work toward the release of an application called Discovery Tooling, which is an online tool in IBM Watson™ Discovery to help the users quickly set up an instance of the service and populate it with data.</p>' +
+				'<p>My role was to build fully functional front-end modules using React alongwith collaborating with UX designers and back-end engineers to bring product features to life. My most significant contribution to the vision of this product was building the conversational feature (Chat Bot) using Discovery API to simplify the operational processes of customers by providing intuitive responses to their user queries.</p>' +
+				'<p>I learned a great deal about writing clean, maintainable, and scalable code on this project. I also made major contributions toward instituting new standards of writing automated integration tests using the Cypress framework. Ocassionally, I participated in company events such as backlog grooming, brainstorming sessions, and several other fun activities focusing on business processes.</p>' +
+				'<p>Currently, I am working with FiCrowd, who provide a white label, crowdfunding solution for banks and credit unions. My role is to develop and architect user interaction for a web application that provides high performance and scalable crowdfunding solutions via its innovative features.</p>',
+			tags: ['React', 'Redux', 'SASS', 'Typescript', 'Node', 'Jest', 'Enzyme', 'Cypress', 'Ruby on rails', 'Git']
+		},
+		{
+			company: 'Capgemini Technology Services India Ltd.',
+			logo: 'assets/images/capgemini-logo.gif',
+			role: 'Senior Analyst',
+			period: ' June 2015 - June 2017',
+			location: 'Bangalore, Karnataka, India',
+			responsibilities:
+				'<p>I landed my first job at Capgemini right after completing my undergraduate degree, where I joined as a Software Engineer in the .NET domain. After completing my training in .NET framework at the company learning facility, I joined a small team of 8 developers and worked with Jetstar Airways Pty Ltd.</p>' +
+				'<p>Initially, my role was to develop the front end portion of a web application that was intended to provide ... (to be continued)</p>',
+			tags: ['.NET', 'C#', 'ASP.NET', 'HTML5/CSS3', 'JavaScript', 'jQuery', 'Bootstrap', 'Oracle PL-SQL']
+		}
+	]
 };
