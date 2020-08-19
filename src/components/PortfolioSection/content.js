361879616045a0d11b20c2f412e892be3bec653f
Sahil Johari <darewithsj@gmail.com>
2020-08-19 16:37:34 -0400

    Links added New project added Layout fixes

--- src/components/PortfolioSection/content.js
+++ src/components/PortfolioSection/content.js
@@ -1,53 +1,62 @@
 export const content = {
-	bannerQuote: '“Only those who risk going too far can possibly find out how far one can go.”',
-	bannerQuoteAuthor: '- T.S Eliot',
-	sectionTitle: 'Stuff made with love',
-	projects: [
-		{
-			projectId: 1,
-			projectName: 'Messenger App',
-			projectSummary:
-				'A simple chat application which allows over 10,000 users to chat in a shared space at once. The application uses React and Typescript in the front end, and Node paired with Socket-IO library for the server operations.',
-			externalLink: 'https://github.com/sahiljohari/messenger-app',
-			thumbnail: 'messenger-app.png'
-		},
-		{
-			projectId: 2,
-			projectName: 'G.I.S.M.O',
-			projectSummary:
-				'GISMO simulates a battlefield in which two rival forces compete to win. Using neuro-evolution, the tanks in the game determine their movement and direction of firing, evolving gradually to perform their tasks.',
-			externalLink: 'https://github.com/sahiljohari/gismo',
-			thumbnail: 'gismo.jpg'
-		},
-		{
-			projectId: 3,
-			projectName: 'Snake AI',
-			projectSummary: 'Implemented a type of reinforcement learning algorithm called Q-learning to model an agent which plays the game of Snake.',
-			externalLink: 'https://github.com/sahiljohari/thisthinglearns',
-			thumbnail: 'snake.gif'
-		},
-		{
-			projectId: 4,
-			projectName: 'Wireless Sensor Network',
-			projectSummary:
-				'Partitioning Random Geometric Graphs into Bipartite Subgraphs: Implemented an algorithm that models ad-hoc networks of wireless sensors of varying scale across a variety of geographic areas.',
-			externalLink: 'https://github.com/sahiljohari/wsn',
-			thumbnail: 'wsn.png'
-		},
-		{
-			projectId: 5,
-			projectName: 'Portfolio Website v1',
-			projectSummary:
-				'Static site written in pure ReactJS to present myself to the internet.',
-			externalLink: 'https://github.com/sahiljohari/portfolio-website',
-			thumbnail: 'portfolio.png'
-		},
-		{
-			projectId: 6,
-			projectName: 'Wireless Sensor Network 2.0',
-			projectSummary:
-				'Recreating WSN using P5.js. Stay tuned!',
-			thumbnail: 'work-in-progress.png'
-		}
-	]
+  bannerQuote:
+    "“Only those who risk going too far can possibly find out how far one can go.”",
+  bannerQuoteAuthor: "- T.S Eliot",
+  sectionTitle: "Stuff made with love",
+  projects: [
+    {
+      projectId: 0,
+      projectName: "Visualize Sorting Algorithms",
+      projectSummary:
+        "A collection of p5.js sketches that simulate various sorting algorithms on randomly generated numbers.",
+      demoLink: "https://editor.p5js.org/sahiljohari/collections/1O2870r00",
+      thumbnail: "sorting.png",
+    },
+    {
+      projectId: 1,
+      projectName: "Messenger App",
+      projectSummary:
+        "A simple chat application which allows multiple users to chat in a shared space. The application uses React and Typescript in the front end, and Node paired with Socket-IO library for the server operations.",
+      externalLink: "https://github.com/sahiljohari/messenger-app",
+      thumbnail: "messenger-app.png",
+    },
+    {
+      projectId: 2,
+      projectName: "G.I.S.M.O",
+      projectSummary:
+        "GISMO simulates a battlefield in which two rival forces compete to win. Using neuro-evolution, the tanks in the game determine their movement and direction of firing, evolving gradually to perform their tasks.",
+      externalLink: "https://github.com/sahiljohari/gismo",
+      thumbnail: "gismo.jpg",
+    },
+    {
+      projectId: 3,
+      projectName: "Snake AI",
+      projectSummary:
+        "The classic Snake game played by an AI agent which uses Deep Reinforcement Learning technique to learn and play the game.",
+      externalLink: "https://github.com/sahiljohari/thisthinglearns",
+      thumbnail: "snake.gif",
+    },
+    {
+      projectId: 4,
+      projectName: "Wireless Sensor Network",
+      projectSummary:
+        "An algorithm that models ad-hoc networks of wireless sensors of varying scale across a variety of geographic areas by partitioning randomly generated geometric graphs into bipartite subgraphs.",
+      externalLink: "https://github.com/sahiljohari/wsn",
+      thumbnail: "wsn.png",
+    },
+    {
+      projectId: 5,
+      projectName: "Portfolio Website v1",
+      projectSummary:
+        "Static site written in ReactJS to present myself to the internet.",
+      externalLink: "https://github.com/sahiljohari/portfolio-website",
+      thumbnail: "portfolio.png",
+    },
+    {
+      projectId: 6,
+      projectName: "Wireless Sensor Network 2.0",
+      projectSummary: "Recreating WSN using P5.js. Stay tuned!",
+      thumbnail: "work-in-progress.png",
+    },
+  ],
 };
