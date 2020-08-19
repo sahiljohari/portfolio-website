5e70bf80ffe56bc1436470ec0e7199c967dcc860
sahiljohari <darewithsj@gmail.com>
2020-08-19 16:39:48 -0400

    Links added (#8)
    New project added
    Layout fixes

--- src/components/PortfolioSection/ProjectCard.js
+++ src/components/PortfolioSection/ProjectCard.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import styles from './ProjectCard.module.css';
+import React from "react";
+import styles from "./ProjectCard.module.css";
 
 const ProjectCard = ({ projectContent, className }) => {
-	const { projectName, projectSummary, externalLink, thumbnail } = projectContent;
+  const {
+    projectName,
+    projectSummary,
+    externalLink,
+    demoLink,
+    thumbnail,
+  } = projectContent;
 
-	return (
-		<div className={className}>
-			<div className={styles.projectCardImgContainer}>
-				<img className={styles.projectCardImg} src={`assets/images/${thumbnail}`} alt="messenger-app"></img>
-			</div>
-			<div className={styles.projectCardDescription}>
-				<h4 className={styles.projectTitle}>{projectName}</h4>
-				<p className={styles.projectSummary}>{projectSummary}</p>
-			</div>
-			{externalLink && <a className={styles.buttonGithub} href={externalLink} target="_blank" rel="noopener noreferrer">
-				<i className={`fab fa-github ${styles.githubIcon}`}></i>
-			</a>
-}
-		</div>
-	);
+  return (
+    <div className={className}>
+      <div className={styles.projectCardImgContainer}>
+        <img
+          className={styles.projectCardImg}
+          src={`assets/images/${thumbnail}`}
+          alt="messenger-app"
+        ></img>
+      </div>
+      <div className={styles.projectCardDescription}>
+        <h4 className={styles.projectTitle}>{projectName}</h4>
+        <p className={styles.projectSummary}>{projectSummary}</p>
+      </div>
+      {demoLink && (
+        <a
+          className={styles.buttonGithub}
+          href={demoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={`fas fa-eye ${styles.githubIcon}`}></i>
+        </a>
+      )}
+      {externalLink && (
+        <a
+          className={styles.buttonGithub}
+          href={externalLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={`fab fa-github ${styles.githubIcon}`}></i>
+        </a>
+      )}
+    </div>
+  );
 };
 
 export default ProjectCard;
