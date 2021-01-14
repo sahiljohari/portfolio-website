d6c9bb4932d6f5950bfb27c842539ba209e59ef8
sahiljohari <darewithsj@gmail.com>
2021-01-14 16:32:57 -0500

    Content and styling updated for about (#12)

--- src/components/AboutSection/content.js
+++ src/components/AboutSection/content.js
@@ -1,14 +1,13 @@
 export const content = {
   sectionTitle: "About me",
   aboutText:
-    "<p>Hello! I'm Sahil, a software engineer based in Pittsburgh, PA who enjoys building things that live on the internet. I develop exceptional web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends.</p>" +
-    "<p>Shortly after graduating from <a href='https://www.smu.edu/' target='_blank' rel='noopener noreferrer'>Southern Methodist University</a> with a Masters degree in Computer Science, I joined the engineering team at <a href='https://www.riversagile.com/' target='_blank' rel='noopener noreferrer'>Rivers Agile</a> where I work on a wide variety of interesting and meaningful projects on a daily basis.</p>",
+    "<p>Hello! I'm Sahil and I am a software engineer. I have been developing highly available, scalable, and real time websites and applications with pixel-perfect user interfaces for over <strong>3.5 years</strong>. My experience lies in full-stack development with primary focus on the front end. For the past year, I have been developing software with engaging user experience and modern look-and-feel across multiple platforms (web/mobile).</p>",
   extendedAboutText:
-    "<p>Completing <strong>3 years</strong> of my career in the software industry, I have learned a lot of skills along the way. Beginning my career as a Software Engineer at Capgemini Technology Services India Limited, I worked on end-to-end web applications for 2 years.</p>" +
-    "<p>Outside of work, I like to spend my time working on side-projects and playing video games. I also enjoy solving coding problems that involve great analytical thinking and challenge my brain cells at every step.</p>" +
-    "<p>I am an explorer, and tend to constantly look for new things to learn. These days, I am exploring <i>creative computing</i> where I create algorithm visualizations to see them in action and play around with them. Check out my projects in the Portfolio section while you are here!</p>",
+  "<p>Shortly after graduating from <a href='https://www.smu.edu/' target='_blank' rel='noopener noreferrer'>Southern Methodist University</a> with a Masters degree in Computer Science, I joined the engineering team at <a href='https://www.riversagile.com/' target='_blank' rel='noopener noreferrer'>Rivers Agile</a> where I work on a wide variety of interesting and meaningful projects on a daily basis.</p>"+  
+  "<p>Outside of work, I like to spend my time working on side-projects and playing video games. I also enjoy solving coding problems that involve great analytical thinking and challenge me intellectually at every step.</p>" +
+    "<p>I am an explorer, and tend to constantly look for new things to learn and build. Check out my projects in the Portfolio section while you are here!</p>",
   technologies: {
-    titleText: "Here are a few technologies I've been working with recently:",
+    titleText: "As an engineer, here are some of the technologies I have worked with so far in my career:",
     techStack: [
       "HTML",
       "React",
@@ -17,6 +16,8 @@ export const content = {
       "JavaScript (ES6+)",
       "Node",
       "TypeScript",
+      "Python",
+      "C#",
       "Socket-IO",
       "Cypress",
       "Jest/Enzyme",
@@ -24,6 +25,9 @@ export const content = {
       "REST API",
       "AWS Lambda",
       "Angular 9",
+      "Firebase",
+      "Oracle PL-SQL",
+      "MySQL"
     ],
   },
 };
