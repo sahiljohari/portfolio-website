8d7baaa830067a97e00ef3550707e11a1e80328e
Sahil Johari <darewithsj@gmail.com>
2021-01-17 11:48:25 -0500

    Created Grid and Card components

--- src/components/PortfolioSection/PortfolioSection.js
+++ src/components/PortfolioSection/PortfolioSection.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { ReactSVG as SVG } from 'react-svg';
+import { ReactSVG as SVG } from "react-svg";
 import { content } from "./content";
-import ProjectCard from "./ProjectCard";
+import Grid from "../common/Grid/Grid";
+import Card from "../common/Card/Card";
 import "./style.css";
 
 const PortfolioSection = () => {
@@ -9,21 +10,37 @@ const PortfolioSection = () => {
   return (
     <div className="portfolio-container">
       <div className="banner">
-        <SVG src={'assets/svgs/undraw_maker_launch_crhe.svg'} className={'portfolio-svg'} />
-        <div className="bannerQuote">{content.bannerQuote}
+        <SVG
+          src={"assets/svgs/undraw_maker_launch_crhe.svg"}
+          className={"portfolio-svg"}
+        />
+        <div className="bannerQuote">
+          {content.bannerQuote}
           <div className="bannerQuoteAuthor">{content.bannerQuoteAuthor}</div>
         </div>
       </div>
       <h2 className="portfolioSectionTitle">{sectionTitle}</h2>
-      <div className="projects-grid">
-        {projects.map((project) => (
-          <ProjectCard
-            projectContent={project}
-            className={"project-card"}
-            key={project.projectId}
-          />
-        ))}
-      </div>
+      <Grid>
+        {projects.map(
+          ({
+            projectId,
+            projectName,
+            projectSummary,
+            externalLink,
+            demoLink,
+            thumbnail,
+          }) => (
+            <Card
+              key={projectId}
+              displayImg={thumbnail}
+              title={projectName}
+              description={projectSummary}
+              externalLink={externalLink}
+              demoLink={demoLink}
+            />
+          )
+        )}
+      </Grid>
     </div>
   );
 };
---
+++ src/components/common/Card/Card.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './Card.module.css';
+
+const Card = ({displayImg, title, description, externalLink, demoLink}) => {
+    return (
+    <div className={styles.card}>
+        <div className={styles.cardImgContainer}>
+        <img
+          className={styles.cardImg}
+          src={`assets/images/${displayImg}`}
+          alt="messenger-app"
+        ></img>
+      </div>
+      <div className={styles.cardDescription}>
+        <h4 className={styles.title}>{title}</h4>
+        <p className={styles.summary}>{description}</p>
+      </div>
+      {demoLink && (
+        <a
+          className={styles.externalLinkBtn}
+          href={demoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={`fas fa-eye ${styles.btnIcon}`}></i>
+        </a>
+      )}
+      {externalLink && (
+        <a
+          className={styles.externalLinkBtn}
+          href={externalLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={`fab fa-github ${styles.btnIcon}`}></i>
+        </a>
+      )}
+      </div>
+    )
+};
+
+export default Card;
