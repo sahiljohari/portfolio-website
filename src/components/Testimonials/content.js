e31ece86563413ae5e0177c243d3b5c9e95e25b7
Sahil Johari <darewithsj@gmail.com>
2021-01-17 14:37:04 -0500

    Added more testimonials on home page Layout changed to grid New Resume uploaded

--- src/components/Testimonials/content.js
+++ src/components/Testimonials/content.js
@@ -2,41 +2,46 @@ export const sectionTitle = 'Testimonials';
 export const content = [
 	{
 		id: 1,
+		name: 'Dave Beshero',
+		profession: 'UX Lead at IBM',
+		profile: 'https://www.linkedin.com/in/dave-beshero-8b277b52/',
+		image: 'assets/images/dave-beshero.jfif',
+		highlight: "Sahil's curiosity and chill attitude made my time pair programming with him fun and engaging. He is an effective communicator with his words and his code.",
+	},
+	{
+		id: 2,
+		name: 'Jeremy Burton',
+		profession: 'Sr. UX Designer at IBM',
+		profile: 'https://www.linkedin.com/in/jeremygburton/',
+		image: 'assets/images/jeremy-burton.jfif',
+		highlight: "Sahil clearly brings great care to his work, both the craft that it takes to bring to life the front-end of a complex application and also the experience that users ultimately have with it."
+	},
+	{
+		id: 3,
 		name: 'Nick Mele',
 		profession: 'Software Engineer at IBM',
 		profile: 'https://www.linkedin.com/in/nick-mele/',
 		image: 'assets/images/nick-mele.jpg',
 		highlight: 'My favorite part about working with Sahil, was the confidence I had to give him a task and know that he would successfully complete it.',
-		testimony:
-			'I worked with Sahil directly when he was on a project with me at IBM. ' +
-			'The eagerness and drive that he displayed to learn was incredible. ' +
-			'He was also an extremely quick learner, which paired well with the drive that he had. ' +
-			"He wasn't afraid to ask questions, but he would never ask the same question twice. " +
-			'To top it all off, he was just a really good person who I enjoyed being around at the office. I would welcome working with Sahil again in the future, and strongly encourage anyone else who has the opportunity to work with him.'
+		
 	},
 	{
-		id: 2,
+		id: 4,
 		name: 'Keith Jordan',
 		profession: 'Software Engineer at Rivers Agile',
 		profile: 'https://www.linkedin.com/in/keith-jordan-64b0642b/',
 		image: 'assets/images/keith-jordan.jpg',
-		highlight: 'Sahil is very patient, no matter how complex the issue, and he often seeks a simple solution.',
-		testimony:
-			'Sahil has an extremely positive attitude.  No matter how much pressure we’re under, or what difficulties we have to endure, he stays calm and focused. ' +
-			'We have paired on several tasks, and I’ve always enjoyed working with Sahil. ' +
-			'He is very patient, no matter how complex the issue, and he often seeks a simple solution. ' +
-			'Anything Sahil initially has difficulty with, he patiently works at until he understands it.'
+		highlight: 'Sahil has an extremely positive attitude.  No matter how much pressure we’re under, or what difficulties we have to endure, he stays calm and focused. He is very patient, no matter how complex the issue, and he often seeks a simple solution.',
+		
 	},
+	
 	{
-		id: 3,
-		name: 'Alexa Taylor',
-		profession: 'Sr. Associate Director at Southern Methodist University',
-		profile: 'https://www.linkedin.com/in/alexataylor/',
-		image: 'assets/images/alexa-taylor.jpg',
-		highlight: 'Sahil was dependable, careful, and always willing to work when needed.',
-		testimony:
-			'Sahil served as a student proctor during his time as a graduate student at SMU. ' +
-			' He was dependable, careful, and always willing to work when needed. ' +
-			' He will make a great addition to any team!'
-	}
+		id: 5,
+		name: 'Savita Nagaraj',
+		profession: 'Staff Web Test Engineer at ZOLL Medical Corporation',
+		profile: 'https://www.linkedin.com/in/savitanagaraj/',
+		image: 'assets/images/savita-nagaraj.jfif',
+		highlight: "Sahil is a great team player and has many a time helped me understand so many JavaScript concepts and in a way has also mentored me while working together on a few assignments."
+	},
+	
 ];
