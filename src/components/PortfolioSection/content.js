6b62b58d4d44c3d90cb2fb6aed2cac5daab6ef62
Sahil Johari <darewithsj@gmail.com>
2021-03-27 13:15:54 -0400

    Added new project Added Updated resume

--- src/components/PortfolioSection/content.js
+++ src/components/PortfolioSection/content.js
@@ -6,6 +6,15 @@ export const content = {
   projects: [
     {
       projectId: 0,
+      projectName: "ExpiryMinder",
+      projectSummary:
+        "A basic organizing and tracking web app that helps you keep track of expiration of documents like travel, finance, business, etc.",
+      externalLink: "https://github.com/sahiljohari/document-organizer",
+      demoLink: "https://document-organizer.vercel.app/",
+      thumbnail: "expiryminder.png",
+    },
+    {
+      projectId: 1,
       projectName: "Visualize Sorting Algorithms",
       projectSummary:
         "A collection of p5.js sketches that simulate various sorting algorithms on randomly generated numbers.",
@@ -13,7 +22,7 @@ export const content = {
       thumbnail: "sorting.png",
     },
     {
-      projectId: 1,
+      projectId: 2,
       projectName: "Messenger App",
       projectSummary:
         "A simple chat application which allows multiple users to chat in a shared space. The application uses React and Typescript in the front end, and Node paired with Socket-IO library for the server operations.",
@@ -21,7 +30,7 @@ export const content = {
       thumbnail: "messenger-app.png",
     },
     {
-      projectId: 2,
+      projectId: 3,
       projectName: "G.I.S.M.O",
       projectSummary:
         "GISMO simulates a battlefield in which two rival forces compete to win. Using neuro-evolution, the tanks in the game determine their movement and direction of firing, evolving gradually to perform their tasks.",
@@ -29,7 +38,7 @@ export const content = {
       thumbnail: "gismo.jpg",
     },
     {
-      projectId: 3,
+      projectId: 4,
       projectName: "Snake AI",
       projectSummary:
         "The classic Snake game played by an AI agent which uses Deep Reinforcement Learning technique to learn and play the game.",
@@ -37,7 +46,7 @@ export const content = {
       thumbnail: "snake.gif",
     },
     {
-      projectId: 4,
+      projectId: 5,
       projectName: "Wireless Sensor Network",
       projectSummary:
         "An algorithm that models ad-hoc networks of wireless sensors of varying scale across a variety of geographic areas by partitioning randomly generated geometric graphs into bipartite subgraphs.",
@@ -45,20 +54,12 @@ export const content = {
       thumbnail: "wsn.png",
     },
     {
-      projectId: 5,
+      projectId: 6,
       projectName: "Portfolio Website v1",
       projectSummary:
         "Static site written in ReactJS to present myself to the internet.",
       externalLink: "https://github.com/sahiljohari/portfolio-website",
       thumbnail: "portfolio.png",
-    },
-    {
-      projectId: 6,
-      projectName: "Document Organizer",
-      projectSummary:
-        "Currently in-progress. This is a tracking application for all your important documents such as IDs, travel docs, and financial docs. Stay tuned!",
-      externalLink: "https://github.com/sahiljohari/document-organizer",
-      thumbnail: "work-in-progress.png",
     },
   ],
 };
