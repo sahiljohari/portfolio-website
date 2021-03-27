22596fb6e90170e4bad397a567c8f1a02d888d45
sahiljohari <darewithsj@gmail.com>
2021-03-27 13:19:19 -0400

    chore: new project with styling fixes (#16)
    * Work section title size fixed
    
    * Added new project
    Added Updated resume

--- src/components/common/Card/Card.js
+++ src/components/common/Card/Card.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styles from './Card.module.css';
+import React from "react";
+import styles from "./Card.module.css";
 
-const Card = ({displayImg, title, description, externalLink, demoLink}) => {
-    return (
+const Card = ({ displayImg, title, description, externalLink, demoLink }) => {
+  return (
     <div className={styles.card}>
-        <div className={styles.cardImgContainer}>
+      <div className={styles.cardImgContainer}>
         <img
           className={styles.cardImg}
           src={`assets/images/${displayImg}`}
@@ -15,28 +15,30 @@ const Card = ({displayImg, title, description, externalLink, demoLink}) => {
         <h4 className={styles.title}>{title}</h4>
         <p className={styles.summary}>{description}</p>
       </div>
-      {demoLink && (
-        <a
-          className={styles.externalLinkBtn}
-          href={demoLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className={`fas fa-eye ${styles.btnIcon}`}></i>
-        </a>
-      )}
-      {externalLink && (
-        <a
-          className={styles.externalLinkBtn}
-          href={externalLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className={`fab fa-github ${styles.btnIcon}`}></i>
-        </a>
-      )}
+      <div className={styles.externalLinkContainer}>
+        {demoLink && (
+          <a
+            className={styles.externalLinkBtn}
+            href={demoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className={`fas fa-eye ${styles.btnIcon}`}></i>
+          </a>
+        )}
+        {externalLink && (
+          <a
+            className={styles.externalLinkBtn}
+            href={externalLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className={`fab fa-github ${styles.btnIcon}`}></i>
+          </a>
+        )}
       </div>
-    )
+    </div>
+  );
 };
 
 export default Card;
