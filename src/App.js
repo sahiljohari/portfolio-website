8846c57648d7f291836c818e231d5067ecdf46c1
sahiljohari <darewithsj@gmail.com>
2021-06-27 19:01:16 -0400

    Responsiveness fixed on home page (#25)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import LoadingSpinner from './components/common/LoadingSpinner/LoadingSpinner';
-import { routes } from './routes';
-import './App.css';
+import React, { Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import LoadingSpinner from "./components/common/LoadingSpinner/LoadingSpinner";
+import { routes } from "./routes";
+import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Suspense fallback={<LoadingSpinner isLoading={true} />}>
-					<NavBar />
-					<Switch>{routes.map(route => route.access && <Route key={route.path} path={route.path} exact component={route.component} />)}</Switch>
-				</Suspense>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Suspense fallback={<LoadingSpinner isLoading={true} />}>
+          <NavBar />
+          <Switch>
+            {routes.map(
+              (route) =>
+                route.access && (
+                  <Route
+                    key={route.name}
+                    path={route.path}
+                    exact
+                    component={route.component}
+                  />
+                )
+            )}
+            <Route>
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -1,32 +1,41 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { routes } from '../../routes';
-import { logo } from './content';
-import './style.css';
-import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { routes } from "../../routes";
+import { logo } from "./content";
+import "./style.css";
+import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
 
 const HeaderNav = ({ drawerClickHandler }) => {
-	return (
-		<header className="navbar">
-			<nav className="navbar__navigation">
-				<>
-					<Link className="logo" to="/" dangerouslySetInnerHTML={{ __html: logo }} />
-					<div className="navbar__items">
-						{routes
-							.filter(nav => nav.access)
-							.map((nav, i) => (
-								<div className="nav__item" key={i}>
-									<NavLink to={nav.path} exact activeClassName="nav__item__active">
-										<p>{nav.name}</p>
-									</NavLink>
-								</div>
-							))}
-					</div>
-				</>
-				<DrawerToggleButton click={drawerClickHandler} />
-			</nav>
-		</header>
-	);
+  return (
+    <header className="navbar">
+      <nav className="navbar__navigation">
+        <>
+          <Link
+            className="logo"
+            to="/"
+            dangerouslySetInnerHTML={{ __html: logo }}
+          />
+          <div className="navbar__items">
+            {routes.map(
+              (nav, i) =>
+                nav.access && (
+                  <div className="nav__item" key={i}>
+                    <NavLink
+                      to={nav.path}
+                      exact={true}
+                      activeClassName="nav__item__active"
+                    >
+                      <p>{nav.name}</p>
+                    </NavLink>
+                  </div>
+                )
+            )}
+          </div>
+        </>
+        <DrawerToggleButton click={drawerClickHandler} />
+      </nav>
+    </header>
+  );
 };
 
 export default HeaderNav;
