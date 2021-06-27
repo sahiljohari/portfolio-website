9b00f2312f4f16e626bf6d3cb400de172565eb57
Sahil Johari <darewithsj@gmail.com>
2021-06-27 11:38:26 -0400

    Responsiveness fixed on home page - Default routing added - Routes fixed for mobile view - Replaced favicon

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { routes } from '../../routes';
-import './style.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { routes } from "../../routes";
+import "./style.css";
 
-const SideDrawer = ({show, optionClick}) => (
-	<nav className={show ? 'side-drawer open' : 'side-drawer'}>
-		<div>
-			{routes.map((nav, i) => (
-				<div key={i}>
-					<NavLink to={nav.path} onClick={optionClick}>{nav.name}</NavLink>
-				</div>
-			))}
-		</div>
-	</nav>
+const SideDrawer = ({ show, optionClick }) => (
+  <nav className={show ? "side-drawer open" : "side-drawer"}>
+    <div>
+      {routes.map(
+        (nav, i) =>
+          nav.access && (
+            <div key={i}>
+              <NavLink to={nav.path} exact={true} onClick={optionClick}>
+                {nav.name}
+              </NavLink>
+            </div>
+          )
+      )}
+    </div>
+  </nav>
 );
 
 export default SideDrawer;
